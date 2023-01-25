83c3d6454bd21cce52a1542ace93a220cb78ed2c
Kareem Kamal <34511976+eskiimo@users.noreply.github.com>
2023-01-25 22:03:07 +0200

    Added calender to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import ReusableNav from "./components/nav";
+import Calender from "./components/calender";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <ReusableNav links={["home", "Contact"]} title="krem" />
+        <Calender />
       </Router>
     </div>
   );
